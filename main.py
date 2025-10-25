febef776c613e1f6d5ca9ebca8241c6753f24e84
YashRaj1506 <yashraj504300@gmail.com>
2025-10-25 17:18:28 +0530

    added logger setup

--- main.py
+++ main.py
@@ -2,6 +2,28 @@ from Scraper.html_scraper import HTMLScraper
 from Scraper.js_scraper import JSScraper
 from verify_content.page_analyzer import PageAnalyzer
 from Scraper.url_manager import URLManager, RedisClient
+import logging
+
+logging.basicConfig(
+    filename="app.log",
+    level=logging.INFO,
+    format="%(asctime)s - %(levelname)s - %(message)s"
+)
+
+logger =logging.getLogger(__name__)
+
+formatter = logging.Formatter("%(asctime)s - %(levelname)s - %(message)s")
+
+#File handler
+file_handler = logging.FileHandler("app.log")
+file_handler.setFormatter(formatter)
+
+# Console handler
+console_handler = logging.StreamHandler()
+console_handler.setFormatter(formatter)
+
+logger.addHandler(file_handler)
+logger.addHandler(console_handler)
 
 class MainScraper:
     def __init__(self):
@@ -23,7 +45,8 @@ class MainScraper:
             content = scraper.get_content()
             print(content)
 
-        print(f"[+] Scraped content from {url} (length: {len(content)})")
+        # print(f"[+] Scraped content from {url} (length: {len(content)})")
+        logger.info(f"[+] Scraped content from {url} (length: {len(content)})")
         return content
 
     def close(self):
